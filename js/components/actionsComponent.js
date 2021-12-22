e1a8a764655d36da72ccc33cb9fafec2766866c2
Andrea Ruiz Penagos <aruizpenagos@gmail.com>
2021-12-22 18:31:49 -0500

    feat: options images

--- js/components/actionsComponent.js
+++ js/components/actionsComponent.js
@@ -1,6 +1,6 @@
 export const actionsComponent = ` 
 <div id="btn-options" class="options">
-    <button class="btn" data-selection="0" type="button"><img class="image" src="./assest/rock.jpg" alt="rock"></button>
-    <button class="btn" data-selection="1" type="button"><img class="image" src="./assest/paper.jpg" alt="paper"></button>
-    <button class="btn" data-selection="2" type="button"><img class="image" src="./assest/scissor.jpg" alt="scissor"></button>
+    <button class="btn" data-selection="0" type="button"><img class="image" src="./assets/rock.jpg" alt="rock"></button>
+    <button class="btn" data-selection="1" type="button"><img class="image" src="./assets/paper.jpg" alt="paper"></button>
+    <button class="btn" data-selection="2" type="button"><img class="image" src="./assets/scissor.jpg" alt="scissor"></button>
 </div>`;
